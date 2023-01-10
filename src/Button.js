038e2f8824ae92aea5690904cd914e4dc5817093
Ella <choi.sh@wmakers.kr>
2023-01-10 12:08:14 +0900

    aa

--- src/Button.js
+++ src/Button.js
@@ -4,18 +4,11 @@ function Button({ id, title, color, onClick }) {
       type="button"
       id={id}
       className={
-        "text-center w-1/4 border p-2 bg-" +
-        color +
-        "-500 hover:bg-" +
-        color +
-        "-600 active:bg-" +
-        color +
-        "-700 focus:outline-none focus:ring focus:ring-" +
-        color +
-        "-300 rounded-full text-white"
+        `text-center w-1/4 border p-2 bg-${color}-500 rounded-full text-white hover:bg-${color}-600 active:bg-${color}-700 focus:outline-none focus:ring focus:ring-${color}-300 rounded-full text-white`
       }
       onClick={onClick}
     >
+      {console.log(color)}
       {title}
     </button>
   );
